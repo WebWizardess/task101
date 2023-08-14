ed66ddc9e064428289ea1afdd29a9becbf377f65
911kavya119@gmail.com <911kavya119@gmail.com>
2023-08-14 21:55:13 +0530

    Initial commit

---
+++ first-task/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import RegistrationForm from './page.jsx'; // Make sure the path is correct based on your project structure
+import './style.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1>Add sender information</h1> */}
+      <main>
+        <RegistrationForm />
+      </main>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
---
+++ first-task/src/page.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
+
+
+function RegistrationForm() {
+  const [firstName, setFirstName] = useState('');
+  const [email, setEmail] = useState('');
+  
+  const [showEmailConfirmation, setShowEmailConfirmation] = useState(false); // New state variable
+
+
+  const handleContinue = () => {
+    // console.log('First Name:', firstName);
+   
+    // console.log('Email:', email);
+    
+    setShowEmailConfirmation(true);
+  };
+
+  return (
+    
+    <div className='form-main'>
+    <div className="links">
+        <a href="#">Email Setup</a>
+        <a href="#">Skip</a>
+      </div>
+     <h1 className='head'>Add sender's information</h1>
+     <p>Choose the senders name and email address that will appear in your subscribers inbox</p>
+      <form >
+        <div className='navy' >
+
+          <label for="fname">Senders Name:</label>
+          <input
+            type="text"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </div>
+        
+        <div className='navy'>
+          <label for="email">Senders email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        
+      </form>
+      <button onClick={handleContinue}>Continue</button>
+      {showEmailConfirmation && (
+        <div className="email-confirmation">
+          <img src="images/emailicon.jpg" alt="Email Icon" />
+          <p className='foots'>Check your email</p>
+          <p className='foot'>To start using klaviyo , activate your account with the link sent to </p>
+          <p className='foot'><FontAwesomeIcon icon={faAngleDown} className="angle-icon" />Didn't recieve an email?</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default RegistrationForm;
